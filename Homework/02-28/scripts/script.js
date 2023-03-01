7265a953a698be3bc1aa106f72cf88b7c6d09656
poonev <poon.ev@northeastern.edu>
2023-02-28 21:00:24 -0500

    commit

--- Homework/02-28/scripts/script.js
+++ Homework/02-28/scripts/script.js
@@ -116,8 +116,8 @@ d3.json("data/world-alpha3.json").then(function(world) {
         "coords": [-71.0589, 42.3601]
       },
       {
-        "name": "London",
-        "coords": [-0.1278, 51.5074]
+        "name": "New Orleans",
+        "coords": [29.9511, -90.0715]
       }
     ]; 
 
